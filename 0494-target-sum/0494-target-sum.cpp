6baecb9bb97d66ad13674dcf4dd79d5cc2a9c2ec
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2023-12-01 22:37:15 +0530

    Time: 11 ms (75.37%), Space: 9.6 MB (62.64%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -32,6 +32,41 @@
 // };
 
 
+// class Solution {
+// public:
+// 	    int findTargetSumWays(vector<int>& nums, int target) {
+//         int sum = 0;
+// 		for(auto num: nums) {
+// 			sum+=num;
+// 		}
+// 		int targetSum = (target + sum);
+// 		if(targetSum%2!=0 || targetSum<0) {
+// 			return 0;
+// 		}
+// 		targetSum/=2;
+// 		vector< vector<int> >dp(2, vector<int>(targetSum+1,0));
+// 		dp[0][0]=1;
+//         int n = nums.size();
+// 		for(int i=1;i<=n;i++) {
+// 			int current = i%2;
+// 			int prev = 1-current;
+// 			for(int j=0;j<=targetSum;j++) {
+// 				if(nums[i-1]<=j) {
+// 					dp[current][j] = (dp[prev][j-nums[i-1]] + dp[prev][j]);
+// }
+// else {
+// 	dp[current][j] = dp[prev][j];
+// }
+// 			}
+// 		}
+//  	return dp[n%2][targetSum];
+		
+		
+//     }
+// };
+
+
+
 class Solution {
 public:
 	    int findTargetSumWays(vector<int>& nums, int target) {
@@ -44,23 +79,19 @@ public:
 			return 0;
 		}
 		targetSum/=2;
-		vector< vector<int> >dp(2, vector<int>(targetSum+1,0));
-		dp[0][0]=1;
+		vector<int>dp(targetSum+1,0);
+		dp[0]=1;
         int n = nums.size();
 		for(int i=1;i<=n;i++) {
-			int current = i%2;
-			int prev = 1-current;
-			for(int j=0;j<=targetSum;j++) {
-				if(nums[i-1]<=j) {
-					dp[current][j] = (dp[prev][j-nums[i-1]] + dp[prev][j]);
-}
-else {
-	dp[current][j] = dp[prev][j];
+			for(int j=targetSum;j>=0;j--) {
+									if(nums[i-1]<=j) {
+					dp[j] = (dp[j] + dp[j-nums[i-1]]);
 }
 			}
 		}
- 	return dp[n%2][targetSum];
+ 	return dp[targetSum];
 		
 		
     }
 };
+
