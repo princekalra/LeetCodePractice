6ce7df93277c5ee436facb9ce34e2e41801548e4
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2023-12-03 12:59:11 +0530

    Time: 1179 ms (67.61%), Space: 7 MB (71.42%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -10,30 +10,47 @@ public:
 		}
 		return true;
 	}
-	int calculateMinCut(string &s, int index, vector<int>&dp) {
-		if(index>=s.length()-1) {
-			return 0;
-			}
-		if(isPalindrome(s,index,s.length()-1)) {
-			return 0;
-		}
-		if(dp[index]!=-1) {
-			return dp[index];
-		}
-		int sol = INT_MAX;
-		for(int k=index;k<s.length();k++) {
-			if(isPalindrome(s,index,k)) {
-				sol = min(sol,1 + calculateMinCut(s,k+1, dp));
-			}
-		}
-		return dp[index] = sol;
-	}
+// 	int calculateMinCut(string &s, int index, vector<int>&dp) {
+// 		if(index>=s.length()-1) {
+// 			return 0;
+// 			}
+// 		if(isPalindrome(s,index,s.length()-1)) {
+// 			return 0;
+// 		}
+// 		if(dp[index]!=-1) {
+// 			return dp[index];
+// 		}
+// 		int sol = INT_MAX;
+// 		for(int k=index;k<s.length();k++) {
+// 			if(isPalindrome(s,index,k)) {
+// 				sol = min(sol,1 + calculateMinCut(s,k+1, dp));
+// 			}
+// 		}
+// 		return dp[index] = sol;
+// 	}
 			
-		
+		int calculateMinCutTab(string &s) {
+			int len = s.length();
+			vector<int>dp(len+1,0);
+			for(int i=len-1;i>=0;i--) {
+                    int sol = INT_MAX;
+                if(isPalindrome(s,i,s.length()-1)) {
+                    dp[i] = 0;
+                    continue;
+                }
+				for(int j=i;j<len;j++) {
+					if(isPalindrome(s,i,j)) {
+				sol =  min(sol,1 + dp[j+1]);
+					}
+				}
+                dp[i] = sol;
+			}
+			return dp[0];
+		}
+
+					
     int minCut(string s) {
-		vector<int>dp(s.length(),-1);
-        int sol = calculateMinCut(s,0,dp);
+        int sol = calculateMinCutTab(s);
 		return sol;
     }
 };
-
