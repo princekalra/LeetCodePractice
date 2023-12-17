5f05e7c10eb4654f741190ab97973ad4b90daf1c
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2023-12-17 22:38:52 +0530

    Time: 211 ms (26.81%), Space: 36.1 MB (12.21%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int numSquares(int n) {
-        vector<int>dp(10001,0);
-	for(int i=0;i<=10000;i++){
-		dp[i] = i;
-	}
+        vector<int>dp(10001,INT_MAX);
+	// for(int i=0;i<=10000;i++){
+	// 	dp[i] = i;
+	// }
+        dp[0]=0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3]= 3;
 	for(int i=2;i*i<=n;i++){
 		for(int j = i*i;j<=n;j++){
 		
 		int square_number = i*i;
-		dp[j] = min(dp[j],dp[j-square_number] + 1);
+            
+		dp[j] = min(dp[j],((dp[j-square_number]==INT_MAX?(j-square_number):(dp[j-square_number])) + 1));
 	}
 		
 	}
