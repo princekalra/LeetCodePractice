42c4aff601c5cf4e015f7c850b5ea0db9f4ee360
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2023-12-26 07:27:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (91.67%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int helper(vector<int>&dp, int n) {
-         if(n==1||n==2){
-          return 1;
-      }
+    int integerBreakHelper(int n, vector<int>&dp){
+        if(n==0||n==1){
+            return 1;
+        }
         if(dp[n]!=-1){
             return dp[n];
         }
-         int sol = 1;
-      for(int i=1;i<=(n/2);i++) {
-          sol = max(sol,(max(i,helper(dp,i))*max(n-i,helper(dp,n-i))));
-      }
-        return dp[n] = sol;
+        int maxProduct = 1;
+       for(int i=1;i<=n;i++){
+          int prod = i*(integerBreakHelper(n-i,dp));
+           maxProduct = max(prod,maxProduct);
+       } 
+        return dp[n] = maxProduct;
+        
     }
     int integerBreak(int n) {
-      vector<int>dp(n+1,-1);
-        return helper(dp,n);
+        vector<int>dp(60,-1);
+        int mx = 1;
+        for(int i=1;i<n;i++){
+            int prod = i* integerBreakHelper(n-i,dp);
+             mx = max(prod,mx);
+        }
+       return mx;
     }
 };
-
