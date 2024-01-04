f9fad0bc3b9019fa60bde0b00ef6f247f09f97e1
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-04 10:36:36 +0530

    Time: 30 ms (41.14%), Space: 24.5 MB (38.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-       
         int len = nums.size();
-         vector<int>sol(len,-1);
+        vector<int>sol(len,-1);
         stack<int>st;
-        
-      for(int i=(2*len-1);i>=0;i--) {
-        while(st.empty()==false && st.top()<=nums[i%len]) {
-            st.pop();
+        for(int i=(2*len-1);i>=0;i--){
+            while(st.empty()==false && st.top()<=nums[i%len]){
+                st.pop();
+            }
+            if(st.empty()==false){
+                sol[i%len] = st.top();
+            }
+            st.push(nums[i%len]);
         }
-         if(st.empty()==false) {
-             sol[i%len] = st.top();
-         }
-          st.push(nums[i%len]);
-      }
         return sol;
     }
 };
+
