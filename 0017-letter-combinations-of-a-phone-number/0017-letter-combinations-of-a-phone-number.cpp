04e796e6bc03b56b0e04e06415e4da17b8375d7d
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-05 18:01:35 +0530

    Time: 0 ms (100.00%), Space: 7 MB (43.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<string>arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void helperForLetterCombinations(string &digits,int i,string str,vector<string>&sol) {
-        if(i>=digits.length()){
+    vector<string>map = {"#","#","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void getAllCombination(string &digits, int index, vector<string>&sol, string str){
+        if(index == digits.length()){
             sol.push_back(str);
             return;
         }
-        string substr = arr[digits[i]-'0'];
-        for(auto c: substr)
-        {
+        string char_map_to_int = map[digits[index]-'0'];
+        for(auto c:char_map_to_int){
             str.push_back(c);
-            helperForLetterCombinations(digits,i+1,str,sol);
+            getAllCombination(digits,index+1,sol,str);
             str.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        
         vector<string>sol;
         if(digits.length()==0){
             return sol;
         }
-        helperForLetterCombinations(digits,0,"",sol);
+        getAllCombination(digits,0,sol,"");
         return sol;
     }
 };
