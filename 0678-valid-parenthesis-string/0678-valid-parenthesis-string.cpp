624b968ff74f3e6df2c602c2859e6e0c952a9e49
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-05 09:46:43 +0530

    Time: 3 ms (35.73%), Space: 6.5 MB (75.17%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,39 +1,65 @@
+// class Solution {
+// public:
+//     bool checkValidString(string s) {
+//         stack<int>st1;
+//         stack<int>st2;
+//         for(int i=0;i<s.length();i++){
+//             if(s[i]=='('){
+//                 st1.push(i);
+//             }
+//             else if(s[i]==')'){
+//                 if(!st1.empty()){
+//                     st1.pop();
+//                 }
+//                 else if(st2.empty()==false){
+//                     st2.pop();
+//                 }
+//                 else{
+//                     return false;
+//                 }
+//             }
+//             else{
+//                 st2.push(i);
+//             }
+//         }
+//         while(st1.empty()==false){
+//             if(st2.empty()){
+//                 return false;
+//             }
+//             if(st1.top()<st2.top()){
+//                 st1.pop();
+//                 st2.pop();
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<int>st1;
-        stack<int>st2;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='('){
-                st1.push(i);
+        int cmin = 0;
+        int cmax = 0;
+        for(auto c:s){
+            if(c=='('){
+                cmin++;
+                cmax++;
             }
-            else if(s[i]==')'){
-                if(!st1.empty()){
-                    st1.pop();
-                }
-                else if(st2.empty()==false){
-                    st2.pop();
-                }
-                else{
-                    return false;
-                }
+            else if(c==')'){
+                cmax--;
+                cmin--;
             }
             else{
-                st2.push(i);
+                cmax++;
+                cmin--;
             }
-        }
-        while(st1.empty()==false){
-            if(st2.empty()){
+            if(cmax<0){
                 return false;
             }
-            if(st1.top()<st2.top()){
-                st1.pop();
-                st2.pop();
-            }
-            else{
-                return false;
-            }
+            cmin = max(cmin,0);
         }
-        return true;
+        return cmin==0;
     }
 };
