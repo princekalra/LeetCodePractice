0e7cf6f62905bc6d3baa8a35dd81774b00168bef
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-06 16:31:18 +0530

    Time: 35 ms (67.89%), Space: 13.7 MB (69.98%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-    int getMaxHistogram(vector<int>&heights){
-        int length = heights.size();
-        vector<int>left(length,-1);
-        vector<int>right(length,length);
-        stack<int>st;
-        for(int i=0;i<length;i++){
-            while(st.empty()==false && heights[st.top()]>=heights[i]){
-                right[st.top()] = i;
-                st.pop();
-            }
-            if(st.empty()){
-                left[i] = -1;
-            }
-            else{
-                left[i] = st.top();
-            }
-            st.push(i);
-        }
-        int area = 0;
-        for(int i=0;i<length;i++){
-            area = max(area,(right[i]-left[i]-1)*heights[i]);
-        }
-        return area;
-    }
-    //  int getMaxHistogram(vector<int>&heights){
+    // int getMaxHistogram(vector<int>&heights){
     //     int length = heights.size();
+    //     vector<int>left(length,-1);
+    //     vector<int>right(length,length);
     //     stack<int>st;
-    //      int max_area = 0;
-    //     for(int i=0;i<=length;i++){
-    //         int height = i==length?0:heights[i];
-    //         while(st.empty()==false && (i==length || heights[st.top()]>=heights[i])){
-    //             height = heights[st.top()];
+    //     for(int i=0;i<length;i++){
+    //         while(st.empty()==false && heights[st.top()]>=heights[i]){
+    //             right[st.top()] = i;
     //             st.pop();
-    //              int width = 0;
+    //         }
     //         if(st.empty()){
-    //             width = i;
+    //             left[i] = -1;
     //         }
     //         else{
-    //             width = i-st.top()-1;
-    //         }
-    //         max_area = max(max_area,width*height);
+    //             left[i] = st.top();
     //         }
     //         st.push(i);
     //     }
-    //    return max_area;
+    //     int area = 0;
+    //     for(int i=0;i<length;i++){
+    //         area = max(area,(right[i]-left[i]-1)*heights[i]);
+    //     }
+    //     return area;
     // }
+     int getMaxHistogram(vector<int>&heights){
+        int length = heights.size();
+        stack<int>st;
+         int max_area = 0;
+        for(int i=0;i<=length;i++){
+            int height = i==length?0:heights[i];
+            while(st.empty()==false && (i==length || heights[st.top()]>heights[i])){
+                height = heights[st.top()];
+                st.pop();
+                 int width = 0;
+            if(st.empty()){
+                width = i;
+            }
+            else{
+                width = i-st.top()-1;
+            }
+            max_area = max(max_area,width*height);
+            }
+            st.push(i);
+        }
+       return max_area;
+    }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int length_col = matrix[0].size();
         int length_row = matrix.size();
