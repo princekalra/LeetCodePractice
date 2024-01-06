62ccf6bbd576da1072be0fa484b2d0ce95d0eac1
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-06 09:18:07 +0530

    Time: 12 ms (49.67%), Space: 18.1 MB (47.22%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+  
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+       vector<int>sol;
+        stack<int>st;
+        for(auto asteroid: asteroids){
+            if(asteroid>0){
+                st.push(asteroid);
+            }
+            else{
+                if(st.empty()){
+                    sol.push_back(asteroid);
+                }
+                else{
+                    bool isVanished = false;
+                    while(st.empty()==false && isVanished == false){
+                        int top_asteroid = st.top();
+                        if(abs(top_asteroid)==abs(asteroid)){
+                            st.pop();
+                            isVanished = true;
+                        }
+                        else if(abs(top_asteroid)>abs(asteroid)){
+                            isVanished = true;
+                        }
+                        else{
+                            st.pop();
+                        }
+                    }
+                    if(isVanished == false){
+                        sol.push_back(asteroid);
+                    }
+                }
+            }
+           
+        }
+        vector<int>temp;
+        while(st.empty()==false){
+            temp.push_back(st.top());
+            st.pop();
+        }
+        reverse(temp.begin(),temp.end());
+        for(auto num:temp){
+            sol.push_back(num);
+        }
+        return sol;
+    }
+};
