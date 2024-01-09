5b915377af7110720b10409984a37521034e9120
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-09 12:23:35 +0530

    Time: 62 ms (75.04%), Space: 14.7 MB (75.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -3,14 +3,19 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int>dp(amount+1,1e4+1);
         dp[0] = 0;
+        sort(coins.begin(),coins.end(),greater<int>());
         for(int i=0;i<coins.size();i++){
+            cout<<coins[i]<<endl;
             for(int j=1;j<=amount;j++){
                 if(coins[i]<=j && dp[j]>(1+dp[j-coins[i]])){
                     dp[j] = 1+dp[j-coins[i]];
                 }
             }
-        }
+          
+            }
+        
         if(dp[amount]>=1e4+1){
+           
             return -1;
         }
         return dp[amount];
