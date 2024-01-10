40a9e7b75bcb286dab74a841fff1421d5079a6de
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-10 21:17:14 +0530

    Time: 15 ms (71.65%), Space: 12.1 MB (14.95%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-    long long sumOfNumsWhichAreLargest(vector<int>&nums){
-        stack<int>st;
-        vector<long long>left(nums.size(),0);
-        vector<long long>right(nums.size(),0);
-        for(int i=0;i<nums.size();i++){
-            while(st.empty()==false && nums[st.top()]<nums[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                left[i] = i+1;
-            }
-            else{
-                left[i] = i-st.top();
-            }
-            st.push(i);
-        }
-        while(st.empty()==false){
-            st.pop();
-        }
-        for(int i=nums.size()-1;i>=0;i--){
-            while(st.empty()==false && nums[st.top()]<=nums[i]){
-                st.pop();
-            }
-            if(st.empty()){
-                right[i] = nums.size()-i;
-            }
-            else{
-                right[i] = st.top()-i;
-            }
-            st.push(i);
-        }
-        long long sum = 0;
-        for(int i=0;i<nums.size();i++){
-            // cout<<left[i]<<" "<<right[i]<<endl;
-            long long prod = left[i]*right[i];
-            prod = prod*nums[i];
-            sum+=prod;
-        }
-        return sum;
-    }
-     long long sumOfNumsWhichAreSmallest(vector<int>&nums){
+    // long long sumOfNumsWhichAreLargest(vector<int>&nums){
+    //     stack<int>st;
+    //     vector<long long>left(nums.size(),0);
+    //     vector<long long>right(nums.size(),0);
+    //     for(int i=0;i<nums.size();i++){
+    //         while(st.empty()==false && nums[st.top()]<nums[i]){
+    //             st.pop();
+    //         }
+    //         if(st.empty()){
+    //             left[i] = i+1;
+    //         }
+    //         else{
+    //             left[i] = i-st.top();
+    //         }
+    //         st.push(i);
+    //     }
+    //     while(st.empty()==false){
+    //         st.pop();
+    //     }
+    //     for(int i=nums.size()-1;i>=0;i--){
+    //         while(st.empty()==false && nums[st.top()]<=nums[i]){
+    //             st.pop();
+    //         }
+    //         if(st.empty()){
+    //             right[i] = nums.size()-i;
+    //         }
+    //         else{
+    //             right[i] = st.top()-i;
+    //         }
+    //         st.push(i);
+    //     }
+    //     long long sum = 0;
+    //     for(int i=0;i<nums.size();i++){
+    //         // cout<<left[i]<<" "<<right[i]<<endl;
+    //         long long prod = left[i]*right[i];
+    //         prod = prod*nums[i];
+    //         sum+=prod;
+    //     }
+    //     return sum;
+    // }
+     long long sumOfNumsWhichAreSmallest(vector<int>&nums,bool isLargest){
         stack<int>st;
         vector<int>left(nums.size(),0);
         vector<int>right(nums.size(),0);
         for(int i=0;i<nums.size();i++){
-            while(st.empty()==false && nums[st.top()]>nums[i]){
+            while(st.empty()==false && ((isLargest==false&&nums[st.top()]>nums[i]) ||(isLargest &&nums[st.top()]<nums[i]))){
                 st.pop();
             }
             if(st.empty()){
@@ -60,7 +60,7 @@ public:
             st.pop();
         }
         for(int i=nums.size()-1;i>=0;i--){
-            while(st.empty()==false && nums[st.top()]>=nums[i]){
+            while(st.empty()==false && ((isLargest==false && nums[st.top()]>=nums[i])||(isLargest==true &&(nums[st.top()]<=nums[i]) ))){
                 st.pop();
             }
             if(st.empty()){
@@ -82,10 +82,11 @@ public:
         return sum;
     }
     long long subArrayRanges(vector<int>& nums) {
-        long long sum1 = sumOfNumsWhichAreLargest(nums);
+        long long sum1 = sumOfNumsWhichAreSmallest(nums,true);
         // cout<<sum1<<endl;
-        long long sum2 = sumOfNumsWhichAreSmallest(nums);
+        long long sum2 = sumOfNumsWhichAreSmallest(nums,false);
         // cout<<sum2<<endl;
         return sum1-sum2;
+        return 0;
     }
 };
