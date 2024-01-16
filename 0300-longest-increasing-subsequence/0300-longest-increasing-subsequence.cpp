31746c40e1393ffbc106a38d730adc53523bd603
Prince Kalra <51924097+princekalra@users.noreply.github.com>
2024-01-17 05:22:23 +0530

    Time: 10 ms (73.85%), Space: 10.7 MB (77.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,46 +1,17 @@
-// class Solution {
-// public:
-//     int lengthOfLIS(vector<int>& nums) {
-//         int len = nums.size();
-// 		vector<int>dp(len+1,1);
-// 		dp[0] = 1;
-// 		for(int i=1;i<=len;i++)	{
-// 		for(int j=i+1;j<=len;j++) {
-// 			if(nums[j-1]>nums[i-1] ) {
-// dp[j] = max(dp[j],1+dp[i]);
-// }
-         
-// }
-             
-
-//     }
-//         int sol = 0;
-//         for(auto x:dp) {
-//             sol = max(sol,x);
-//         }
-//         return sol; 
-//     }      
-// };
-
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-       int len = nums.size();
-	  vector<int>dp(len+1,INT_MAX);
-	  for(int i=0;i<len;i++) {
-		int idx = lower_bound(dp.begin(),dp.end(),nums[i])-dp.begin();
-		dp[idx] = nums[i];
-		}
-	int sol = 0;
-for(int i=0;i<=len;i++) {
-if(dp[i]!=INT_MAX) {
-sol = i+1;
-}
-else {
-break;
-}
-}
-return sol;	
-    }      
+        vector<int>vec;
+        vec.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>vec.back()){
+                vec.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(vec.begin(),vec.end(),nums[i]) - vec.begin();
+                vec[ind] = nums[i];
+            }
+        }
+        return vec.size();
+    }
 };
-
